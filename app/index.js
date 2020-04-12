95362a5639b2a33a40f9354905a5be12b707cbc2
webHero <khonjelwayo@gmail.com>
2020-04-12 17:09:15 +0200

    new 'statechange' evt listener

--- app/index.js
+++ app/index.js
@@ -3,16 +3,23 @@ console.log('Application Started');
 
 if ("serviceWorker" in navigator) {
   
-  navigator.serviceWorker.register("sw.js").then((registration) => {
+  let sw;
+  navigator.serviceWorker.register("/sw.js").then((registration) => {
     console.log("Service Worker Registered \nScope", registration.scope);
-
+    if (registration.installing) {
+      sw = registration.installing
+    } 
     if (registration.active) {
-      console.log('Activated');
-      
-    }
+      sw = registration.active
+    } 
+    console.log(sw.state);
+    
+
   }).catch((err) => {
     console.error(`OOPS ${err}`);
   })
+// 
+  // sw.addEventListener('statechange', (evt) => console.log(evt.target.state))
 
 } else {
   console.log('Please upgrade your browser to support the latest features');
